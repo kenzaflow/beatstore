7a0e615b06b0f5cd958afa529ae392eedf3b4665
Kevin Zaracho <kevin.isap99@gmail.com>
2021-10-14 23:51:36 -0300

    terminé el simulador

--- assets/js/coderhouse.js
+++ assets/js/coderhouse.js
@@ -1,5 +1,3 @@
-// si estás viendo esto significa que me falta terminarlo :'(
-
 class licencia {
   constructor(name, price) {
     this.name = name;
@@ -7,7 +5,7 @@ class licencia {
   }
 
   ObtenerIVA() {
-    return this.price * 1.21;
+    return parseInt(this.price * 1.21);
   }
 }
 
@@ -17,31 +15,32 @@ licencias.push(new licencia("Trackout Lease", 79.99));
 licencias.push(new licencia("Unlimited Lease", 149.99));
 
 function ejecutarCODERHOUSE() {
-  let selLease = parseInt(
-    prompt(
-      "A continuación, selecciona el tipo de licencia que querés:\n\n" +
-        "   1. Limited Lease\n" +
-        "   2. Trackout Lease\n" +
-        "   3. Unlimited Lease"
-    )
-  );
+  let txtAskLeases =
+    "A continuación, selecciona el tipo de licencia que querés:\n\n";
+
+  licencias.forEach((element, index) => {
+    txtAskLeases += "   " + (index + 1) + ". " + element.name + "\n";
+  });
+
+  let selLease = parseInt(prompt(txtAskLeases));
 
   let precioNormal = 0;
 
-  if (selLease >= 1 && selLease <= 3) {
-    switch (selLease) {
-      case 1:
-        precioNormal = 29.99;
-      case 2:
-        precioNormal = 79.99;
-      case 3:
-        precioNormal = 149.99;
-    }
+  if (selLease >= 1 && selLease <= licencias.length) {
+    precioNormal = licencias[selLease - 1].price;
   } else {
     alert("Necesitamos que selecciones una opción para continuar");
   }
 
-  /* if (precioNormal > 0) {
-      
-  } */
+  if (precioNormal > 0) {
+    alert(
+      "Elegiste " +
+        licencias[selLease - 1].name +
+        ", que vale $" +
+        licencias[selLease - 1].price +
+        ".\n" +
+        "Con el impuesto IVA, el precio se va a $" +
+        licencias[selLease - 1].ObtenerIVA()
+    );
+  }
 }
